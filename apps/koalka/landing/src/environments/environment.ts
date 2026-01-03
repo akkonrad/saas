02b0bac6f924eef473fc92a21cd548f520d184bc
Konrad <konrad.kalicinski@volleystation.com>
2026-01-03 09:25:51 +0100

    fix supabase rpoject keys

--- apps/koalka/landing/src/environments/environment.ts
+++ apps/koalka/landing/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   recaptchaSiteKey: '6LerwT4sAAAAAInP22I0kTgAtnrDnVL9zjgPGkng',
-  supabaseUrl: 'https://imdxyuyjxmymsvytyxri.supabase.co',
-  contactFunctionUrl: 'https://imdxyuyjxmymsvytyxri.supabase.co/functions/v1/koalka-contact-form',
+  supabaseUrl: 'https://vxxbipfnpghkeqoqxzbs.supabase.co',
+  contactFunctionUrl: 'https://vxxbipfnpghkeqoqxzbs.supabase.co/functions/v1/koalka-contact-form',
 };
